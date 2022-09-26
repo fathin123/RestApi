c3108836fdc2e26f11f5890000225ba1281f6a88
fathin123 <fathinnaufalia@gmail.com>
2022-09-26 14:21:13 +0700

    evaluasi 4 - restapi login

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,222 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- My CSS-->
+    <link rel="stylesheet" href="style.css">
+
+    <title>PortofolioKu | Fathin Naufaliya</title>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="#">PortofolioKu</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#about">About</a>
+                    <a class="nav-link" href="#projects">Projects</a>
+                    <a class="nav-link" href="#contact">Contact</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!--Akhir-->
+
+    <!-- Jumbotron -->
+    <section class="jumbotron text-center">
+        <img src="img/2041723015.jpg" alt="Fathin Naufaliya" width="120" class="rounded-circle" />
+        <h1 class="display-4">Fathin Naufaliya</h1>
+        <p class="lead">Mahasiswi Politeknik Negeri Malang</p>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffffff" fill-opacity="1"
+                d="M0,288L60,256C120,224,240,160,360,133.3C480,107,600,117,720,112C840,107,960,85,1080,96C1200,107,1320,149,1380,170.7L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z">
+            </path>
+        </svg>
+    </section>
+    <!-- Akhir -->
+
+    <!-- About -->
+    <section id="about">
+        <div class="container">
+            <div class="row text-center mb-3">
+                <div class="col">
+                    <h2>About</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center fs-6 text-center">
+                <div class="col-mb-3"
+                    style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">
+                    <p> Perkenalkan nama saya Fathin Naufaliya, mahasiswi dari Politeknik Negeri Malang. </p>
+                    <p> Saya calon FrontEnd Developer berusia 21 tahun yang berasal dari Bojonegoro, suka dengan hal
+                        yang berkaitan dengan website, desain dan editing. Dengan kemajuan teknologi saat ini, membuat saya ingin masuk ke industri ini. </p>
+                </div>
+            </div>
+
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#d3d3d3" fill-opacity="1"
+                d="M0,192L60,186.7C120,181,240,171,360,192C480,213,600,267,720,277.3C840,288,960,256,1080,224C1200,192,1320,160,1380,144L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z">
+            </path>
+        </svg>
+    </section>
+    <!-- Akhir -->
+
+    <!-- Projects-->
+    <section id="projects">
+        <div class="container">
+            <div class="row text-center mb-3">
+                <div class="col-sm-12">
+                    <h3>Projects</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-6 mb-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="img/8.png" class="card-img-top" alt="project1">
+                                    <p class="card-text"
+                                        style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">
+                                        Project 1 merupakan tugas besar dari mata kuliah
+                                        Desain & Pemrograman Web. Dengan membuat website bebas, dan berkelompok. Tugas
+                                        saya adalah membuat CRUD input Data Pasien menggunakan php dan bootstrap.
+                                        <br>
+                                    <p
+                                        style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">
+                                        Berikut lampiran link github saya:</p>
+                                    <a href="https://github.com/fathin123/rs" class="btn btn-primary">Klik Disini</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 mb-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="img/2.17.png" class="card-img-top" alt="project2">
+                                    <p class="card-text"
+                                        style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">
+                                        Project 2 merupakan tugas dari salah satu mata kuliah
+                                        pemrograman web, dimana tugas tersebut membuat website bebas dan saya memilih
+                                        membuat website list harga masker wajah dengan menggunakan php, bootstrap dan
+                                        laravel.
+                                    <p
+                                        style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">
+                                        Berikut lampiran link github saya:</p>
+                                    <a href="https://github.com/fathin123/QUIZ1_FathinNaufaliya_TI2E"
+                                        class="btn btn-primary">Klik Disini</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 mb-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="img/1.29.png" class="card-img-top" alt="project3">
+                                    <p class="card-text"
+                                        style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">
+                                        Project 3 merupakan tugas praktikum dari salah satu mata kuliah
+                                        pemrograman web, dimana tugas tersebut membuat CRUD data mahasiwa dengan
+                                        menggunakan php, bootstrap dan laravel.
+                                        <br>
+                                    <p
+                                        style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">
+                                        Berikut lampiran link github saya:</p>
+                                    <a href="https://github.com/fathin123/07_praktikum_web_lanjut_mahasiswa"
+                                        class="btn btn-primary">Klik Disini</a>
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="col-sm-6 mb-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="img/image.png" class="card-img-top" alt="project4">
+                                    <p class="card-text"
+                                        style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">
+                                        Project 4 merupakan tugas besar dari salah satu mata kuliah
+                                        proyek2, dimana tugas tersebut membuat website bebas, dan berkelompok. Tugas
+                                        saya disini membuat halaman dashboard dan input data dokter. Web ini menggunakan
+                                        php, bootstrap dan laravel.
+                                        <br>
+                                    <p
+                                        style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">
+                                        Berikut lampiran link github saya:</p>
+                                    <a href="https://github.com/tyonesatriaaa/SIAWANN" class="btn btn-primary">Klik
+                                        Disini</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+    </section>
+    <!-- Akhir -->
+
+    <!-- Contact -->
+    <section id="contact">
+        <div class="container">
+            <div class="row text-center mb-3">
+                <div class="col">
+                    <h4>Contact</h4>
+                </div>
+            </div>
+            <div class="row justify-content-center mb-5">
+                <div class="col-sm-6">
+                    <form>
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Nama</label>
+                            <input type="text" class="form-control" id="name" aria-describedby="name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="pesan" class="form-label">Pesan</label>
+                            <textarea class="form-control" id="pesan" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Kirim</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Akhir -->
+
+    <!-- Footer -->
+    <footer class="bg-dark text-white text-center pb-3">
+        <p> Created by <a href="https://www.instagram.com/fathinaufall/" class="text-white fw-bold"> Fathin
+            Naufaliya</a></p>
+    </footer>
+    <!-- Akhir -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
